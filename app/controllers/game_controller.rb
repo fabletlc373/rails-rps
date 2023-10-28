7db2350978d596fd95dbf28c7b164cde85f0d7bd
fabletlc373 <51000618+fabletlc373@users.noreply.github.com>
2023-10-28 19:10:09 +0000

    initial commit

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,54 @@
+class GameController < ApplicationController
+  
+  def user_played_rock
+    outcomes = ['rock', 'paper', 'scissors']
+    otherhand = outcomes.sample(1)[0]
+    
+    @msg0 = "We played rock!"
+    @msg1 = "They played #{otherhand}!"
+    if otherhand=='scissors'
+      @msg2 = "We won!"
+    elsif otherhand=='rock'
+      @msg2 = "We tied!"
+    else
+      @msg2 = "We lost!"
+    end
+    
+    render({:template => "game_templates/results"})
+
+  end
+
+  def user_played_paper
+    @msg0 = "We played paper!"
+    otherhand = outcomes.sample(1)[0]
+    @msg1 = "They played #{otherhand}!"
+    if otherhand=='rock'
+      @msg2 = "We won!"
+    elsif otherhand=='paper'
+      @msg2 = "We tied!"
+    else
+      @msg2 = "We lost!"
+    end
+
+    render({:template => "game_templates/results"})
+
+  end
+  
+  def user_played_scissors
+    @msg0 = "We played scissors!"
+    otherhand = outcomes.sample(1)[0]
+    @msg1 = "They played #{otherhand}!"
+    if otherhand=='paper'
+      @msg2 = "We won!"
+    elsif otherhand=='scissors'
+      @msg2 = "We tied!"
+    else
+      @msg2 = "We lost!"
+    end
+    render({:template => "game_templates/results"})
+  end
+
+  def go_home
+    render({:template => "game_templates/home"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get("/rock", {:controller => "game", :action => "user_played_rock"})
 
+  get("/", {:controller => "game", :action => "go_home"})
 end
